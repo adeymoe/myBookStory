e8f5597a966b41908e065f9684a091c4e9ed87fa
adeymoe <66586650+adeymoe@users.noreply.github.com>
2025-04-12 15:42:45 -0400

    tyest

--- Backend/server.js
+++ Backend/server.js
@@ -13,25 +13,27 @@ const app = express()
 const port = process.env.PORT || 4000
 
 const allowedOrigins = [
-    'http://localhost:5173',
-    'https://mybookstory.vercel.app/'
-  ];
+  'http://localhost:5173',
+  'https://mybookstory.vercel.app/'
+];
 connectDB()
 connectCloudinary()
 
 //midllewares
 app.use(express.json())
 app.use(cors({
-    origin: allowedOrigins,
-  }));
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
 
 // Api endpoints
 app.use('/api/book', bookRouter)
 app.use('/api/user', userRouter)
 app.use('/api/order', orderRouter)
 
-app.get('/', (req,res)=>{
-    res.send("API Working")
+app.get('/', (req, res) => {
+  res.send("API Working")
 })
 
-app.listen(port, ()=>console.log('Server started on PORT : '+ port))
+app.listen(port, () => console.log('Server started on PORT : ' + port))
